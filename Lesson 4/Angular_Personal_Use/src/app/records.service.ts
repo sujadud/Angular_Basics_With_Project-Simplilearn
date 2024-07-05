e7a2520656b38076dd53140143ec6201486bb0dc
Sujad Ud Doula <sujad.ud@outlook.com>
2024-07-05 22:36:00 +0600

    Working on Demo DI

---
+++ Lesson 4/Angular_Personal_Use/src/app/records.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecordsService {
+  info1: string[] = ["Adam Taylor", 'E234', 'at@abc.net']
+  info2: string[] = ["Shawn Michael", 'E236', 'sm@abc.net']
+  info3: string[] = ["Mark Fisher", 'E264', 'mf@abc.net']
+
+  getinfo1(): string[]{
+    return this.info1
+  }
+
+  getinfo2(): string[]{
+    return this.info2
+  }
+
+  getinfo3(): string[]{
+    return this.info3
+  }
+
+  constructor() { }
+}
