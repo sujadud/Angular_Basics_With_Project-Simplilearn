410e20dcbb85805fc91be047b349b90f62cab905
Sujad Ud Doula <sujad.ud@outlook.com>
2024-07-06 22:40:55 +0600

    Added Lesson 4 DI Demo project

--- Lesson 4/Angular_Personal_Use/src/app/emp-info/emp-info.component.ts
+++ Lesson 4/Angular_Personal_Use/src/app/emp-info/emp-info.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { RecordsService } from '../records.service';
 
 @Component({
   selector: 'app-emp-info',
   templateUrl: './emp-info.component.html',
-  styleUrls: ['./emp-info.component.css']
+  styleUrls: ['./emp-info.component.css'],
+  providers: [RecordsService]
 })
 export class EmpInfoComponent {
+
+  infoRecived1: string[] = [];
+  infoRecived2: string[] = [];
+  infoRecived3: string[] = [];
+
+  getInfoFromServiceClass1(){
+    this.infoRecived1 = this.rservice.getinfo1()
+  }
+
+  getInfoFromServiceClass2(){
+    this.infoRecived2 = this.rservice.getinfo2()
+  }
+
+  getInfoFromServiceClass3(){
+    this.infoRecived3 = this.rservice.getinfo3()
+  }
+
+  /**
+   *
+   */
+  constructor(private rservice: RecordsService) {
+    
+    
+  }
+
+  ngOnInit(): void {
+    
+  }
+
 
 }
