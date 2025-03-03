0a31ee0d1f8fc1b246d0ab489d1b8848bc8a6ac4
MD. Sujad UD <43004887+sujadud@users.noreply.github.com>
2025-03-03 14:43:58 +0600

    Added Project of Angular Basics + .NET Core API

---
+++ Project_Contact_Angular+Asp.net_Core/Contact/Contact/Models/Contacts.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Contact.Models
+{
+    public class Contacts
+    {
+        [Key]
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
